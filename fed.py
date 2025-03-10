a62a327b868efb505aed1a649b2f516ae2285564
eduardomrks <duxphone69@gmail.com>
2025-03-10 13:39:12 -0300

    Update fed.py

--- fed.py
+++ fed.py
@@ -53,8 +53,8 @@ async def enviar_depoimento():
 
         # Botões organizados em uma lista de listas para ficarem um embaixo do outro
         botoes = [
-            [Button.url("🚨JOGAR AGORA🚨", "https://rebrand.ly/MinutosPagantesGoldBet")],
-            [Button.url("🎁CADASTRE-SE 🎁", "https://rebrand.ly/MinutosPagantesGoldBet")]
+            [Button.url("🚨JOGAR AGORA🚨", "https://abre.news/MinutosPagantes")],
+            [Button.url("🎁CADASTRE-SE 🎁", "https://abre.news/MinutosPagantes")]
         ]
 
         # Envia mensagem para cada chat_id na lista
